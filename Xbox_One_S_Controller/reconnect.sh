2aeaaa518c3fb26d53b9b53263f2e59c8e1354bb
Xiaokang Guo <xg590@nyu.edu>
2019-10-24 02:13:41 -0400

    Update reconnect.sh

--- Xbox_One_S_Controller/reconnect.sh
+++ Xbox_One_S_Controller/reconnect.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo bash -c "echo 1 > /sys/module/bluetooth/parameters/disable_ertm"
-sudo bash -c "systemctl start bluetooth"
 sudo bash -c "echo -e 'connect XX:XX:XX:XX:XX:XX\nexit' | bluetoothctl"
