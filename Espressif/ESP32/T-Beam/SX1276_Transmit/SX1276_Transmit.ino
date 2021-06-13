d28fad3326890d564587c97e9110c0cb85f48ef4
Xiaokang Guo <xg590@nyu.edu>
2021-06-13 00:36:13 -0400

    LoRa

---
+++ Espressif/ESP32/T-Beam/SX1276_Transmit/SX1276_Transmit.ino
@@ -0,0 +1,14 @@
+#include <RadioLib.h>
+#include "boards.h"
+SX1276 radio = new Module(RADIO_CS_PIN, RADIO_DI0_PIN, RADIO_RST_PIN, RADIO_BUSY_PIN);
+void setup()
+{
+    initBoard(); delay(1500); 
+                            //  float freq, float bw, uint8_t sf, uint8_t cr, uint8_t syncWord, int8_t power, uint16_t preambleLength, uint8_t gain
+    int state = radio.begin(LoRa_frequency,    125.0,          9,          7, SX127X_SYNC_WORD,           10,                       8,            0); 
+}
+void loop()
+{ 
+    int state = radio.transmit("Hello World!");  
+    delay(1000);
+}
