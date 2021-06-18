39bc9089a8055120da4d45bc7c21ed7c1b38ea49
Xiaokang Guo <xg590@nyu.edu>
2021-06-17 22:19:38 -0400

    RFM95W

--- Board/Espressif/ESP32/T-Beam/SX1276_Transmit/SX1276_Transmit.ino
+++ Board/Espressif/ESP32/T-Beam/SX1276_Transmit/SX1276_Transmit.ino
@@ -5,7 +5,7 @@ void setup()
 {
     initBoard(); delay(1500); 
                             //  float freq, float bw, uint8_t sf, uint8_t cr, uint8_t syncWord, int8_t power, uint16_t preambleLength, uint8_t gain
-    int state = radio.begin(LoRa_frequency,    125.0,          9,          7, SX127X_SYNC_WORD,           10,                       8,            0); 
+    int state = radio.begin(LoRa_frequency,    125.0,          9,          7, SX127X_SYNC_WORD,           17,                       8,            0); 
 }
 void loop()
 { 
